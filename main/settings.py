8d94edb8edd41b6808276b4a0252d1d2484d7ba7
Andres66666 <cya2024773@est.univalle.edu>
2025-04-02 23:44:17 -0400

    Configuración final para producción

--- main/settings.py
+++ main/settings.py
@@ -205,3 +205,5 @@ if not DEBUG:
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
